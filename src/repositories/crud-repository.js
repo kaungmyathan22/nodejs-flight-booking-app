8d5f14c439d4fe725f2c816135c9ea5168681d4c
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-02-05 15:29:29 +0630

    feat(repo): crud repo method added

---
+++ src/repositories/crud-repository.js
@@ -0,0 +1,67 @@
+const { Logger } = require("../config");
+
+class CRUDRepository {
+  constructor(model) {
+    this.model = model;
+  }
+
+  async create(data) {
+    try {
+      const response = await this.model.create(data);
+      return response;
+    } catch (error) {
+      Logger.error("Something went wrong in the crud repo : create");
+      throw error;
+    }
+  }
+
+  async destroy(data) {
+    try {
+      const response = await this.model.destroy({
+        where: {
+          id: data,
+        },
+      });
+      return response;
+    } catch (error) {
+      Logger.error("Something went wrong in the crud repo : destroy");
+      throw error;
+    }
+  }
+
+  async getAll() {
+    try {
+      const response = await this.model.find();
+      return response;
+    } catch (error) {
+      Logger.error("Something went wrong in the crud repo : getAll");
+      throw error;
+    }
+  }
+
+  async get(data) {
+    try {
+      const response = await this.model.findByPk(data);
+      return response;
+    } catch (error) {
+      Logger.error("Something went wrong in the crud repo : get");
+      throw error;
+    }
+  }
+
+  async update(id, data) {
+    try {
+      const response = await this.model.update(data, {
+        where: {
+          id,
+        },
+      });
+      return response;
+    } catch (error) {
+      Logger.error("Something went wrong in the crud repo : get");
+      throw error;
+    }
+  }
+}
+
+module.exports = CRUDRepository;
