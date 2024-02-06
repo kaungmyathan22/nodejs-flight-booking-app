4c87c49225a2c83d3334bf8e85b903fae33a4edc
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-02-06 22:05:58 +0630

    feat(models): cascade property defined

--- src/models/airport.js
+++ src/models/airport.js
@@ -11,14 +11,14 @@ module.exports = (sequelize, DataTypes) => {
       this.belongsTo(models.City, {
         foreignKey: "cityId",
       });
-      // this.hasMany(models.Flight, {
-      //   foreignKey: "departureAirportId",
-      //   onDelete: "CASCADE",
-      // });
-      // this.hasMany(models.Flight, {
-      //   foreignKey: "arrivalAirportId",
-      //   onDelete: "CASCADE",
-      // });
+      this.hasMany(models.Flight, {
+        foreignKey: "departureAirportId",
+        onDelete: "CASCADE",
+      });
+      this.hasMany(models.Flight, {
+        foreignKey: "arrivalAirportId",
+        onDelete: "CASCADE",
+      });
     }
   }
   Airport.init(
